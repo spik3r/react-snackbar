cfbab4afeffd5628a683dd51d1b0d7d8ffaff1ef
kai tait <kai-tait@macolighting.com>
2018-05-31 19:41:47 +0200

    Improved UI and animation

--- src/Snackbar.js
+++ src/Snackbar.js
@@ -3,14 +3,25 @@ import './snackbar.css';
 
 export default class Snackbar extends React.Component {
 
- constructor(props) {
-    super(props);
-    this.state = {addClass: false}
-  }
+    constructor(props) {
+        super(props);
+        this.state = {addClass: false};
 
-  render() {
-        return ( <div className="snackbar">
-                    <img src="/glyphicons-44-group.png" alt="img"/> <div className="message">Some Message</div>
-                 </div>);
-      }
+    }
+    toggle() {
+        this.setState({addClass: true});
+    }
+    render() {
+        let snackbarClass = ['snackbar'];
+        if(this.state.addClass) {
+            snackbarClass.push('snackbar_gone');
+        }
+            return (<div className="snackbar-positioner">
+                        <div className={snackbarClass.join(' ')} >
+                         <img className="snackbar_img" alt="img" src="/group.png"/>
+                            <span className="snackbar_message">123 People booked this Foo Bar  in the last hour</span>
+                            <img className="snackbar_close" alt="close icon" src="/close_white.png" onClick={this.toggle.bind(this)}/>
+                        </div>
+                    </div>);
+    }
 }
